7c7bbd92f12393948f025353e90ed6de631fd0eb
jenny.choi <jenny.choi@kakaocorp.com>
2018-06-02 21:57:02 +0900

    feat: chapter2

---
+++ ChapterArranger/chapter2_.py
@@ -0,0 +1,39 @@
+a = 7
+print(a)
+
+b = a
+print(b)
+
+
+print(123)
+print(+123)
+
+
+bottles = 99
+base = ''
+base += 'current inventory: '
+base += str(bottles)
+print(base)
+
+palindrome = 'A men, \nA plan, \n,A canal'
+print(palindrome)
+
+start = 'Na ' * 4 + '\n'
+middle = 'hey ' * 3 + '\n'
+end = 'GoodBye'
+print(start + middle + end)
+
+letters = '1234122341234test'
+print(len(letters))
+
+setup = 'a duck goes into a bar...'
+print(setup.strip('.'))
+
+print('exercise')
+print(1 * 60 * 60)
+seconds_per_hour = 1 * 60 * 60
+print(seconds_per_hour)
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+print(seconds_per_day / seconds_per_hour)
+print(seconds_per_day // seconds_per_hour)

2ca101d31d25b5aff2423c3bb88690f6655e6be8
jenny.choi <jenny.choi@kakaocorp.com>
2018-06-10 19:13:07 +0900

    feat: 챕터 5장 연습문제 -3

--- chater5/inter.py
+++ chater5/inter.py
@@ -1,7 +1,23 @@
-from zoo import hours
+from zoo import hours as info
+from collections import OrderedDict
+from collections import defaultdict
 
 #import zoo as menagerie
 
 #menagerie.hours()
 
-hours()
+#hours()
+
+info()
+
+plain = {'a': 1, 'b': 2, 'c': 3}
+print(plain)
+
+fancy = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+print(fancy)
+
+dict_of_lists = defaultdict(list)
+dict_of_lists['a'].append('something for a')
+print(dict_of_lists['a'])
+
+
